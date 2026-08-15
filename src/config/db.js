8ccb5d4e2dd3b958c2cdd7a2be0f8cbd5b5f6a31
Charlie282 <pankaj.dev282@gmail.com>
2026-08-15 17:46:28 +0530

    - Changed the Connection String

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect( "mongodb://localhost:27017/aakruti" );
+    await mongoose.connect( "mongodb+srv://pankajdev282_db_user:1ET9YDRS7ddvvMiF@cluster0.e9p9rv4.mongodb.net" );
     console.log("database connected succesfully");
   } catch (error) {
     throw Error("Database connection failed");
