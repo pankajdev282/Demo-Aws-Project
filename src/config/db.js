3c220e7db00a81dc22511d5ed596c9bfb8b9ffae
Charlie282 <pankaj.dev282@gmail.com>
2026-08-15 18:03:41 +0530

    error

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,7 @@ const connectDB = async () => {
     await mongoose.connect( "mongodb+srv://pankajdev282_db_user:1ET9YDRS7ddvvMiF@cluster0.e9p9rv4.mongodb.net" );
     console.log("database connected succesfully");
   } catch (error) {
-    throw Error("Database connection failed", error
-    );
+    throw Error("Database connection failed", error?.message);
   }
 };
 
